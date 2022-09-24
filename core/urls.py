103002c3c61841a842615b1f2ae267de64ee14a9
Nadezhda Khovrich <hope28281@gmail.com>
2022-09-24 21:05:24 +0600

    Добавлен product_view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from products.views import products_view
+from products.views import products_view, product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products_view, name='index'),
-    path('/products', products_view)
+    path('products/', products_view),
+    path('products/<int:pk>/', product_view, name='product_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 def products_view(request):
@@ -7,3 +7,11 @@ def products_view(request):
         'products': products,
     }
     return render(request=request, template_name='index.html', context=context)
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'product.html', context=context)
+    
