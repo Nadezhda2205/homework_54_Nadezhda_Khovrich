a8668488801efcfd1895042f4517e064d11e04c2
Nadezhda Khovrich <hope28281@gmail.com>
2022-09-24 22:14:15 +0600

    Добавлена category_add_view

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from products.views import products_view, product_view
+from products.views import products_view, product_view, category_add_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products_view, name='index'),
     path('products/', products_view),
-    path('products/<int:pk>/', product_view, name='product_detail')
+    path('products/<int:pk>/', product_view, name='product_detail'),
+    path('categories/add/', category_add_view, name='category_add'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from products.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product, Category
+from django.core.handlers.wsgi import WSGIRequest
 
 def products_view(request):
     products = Product.objects.all()
@@ -14,4 +15,14 @@ def product_view(request, pk):
         'product': product
     }
     return render(request, 'product.html', context=context)
-    
+
+
+def category_add_view(request: WSGIRequest):
+    if request.method == 'POST':
+        category_data = {
+            'name': request.POST.get('name'),
+            'description': request.POST.get('description'),
+        }
+        Category.objects.create(**category_data)
+        return redirect('index')
+    return render(request=request, template_name='category_add.html')
